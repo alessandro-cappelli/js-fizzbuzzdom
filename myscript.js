cf50f006d11f0a08b7f0994d2fa4cedcd90aa8d7
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-10 16:08:52 +0100

    box

--- myscript.js
+++ myscript.js
@@ -2,5 +2,5 @@ let scatola = document.getElementById("container");
 
 for(let i = 1; i <= 100; i++){
     console.log('iterazione: ', i);
-
+    scatola.innerHTML += '<div class=box' + i + '</div>';
 }
