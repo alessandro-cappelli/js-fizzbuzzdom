b411fa704a378bedce0676d64a5c1d7423435ef2
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-10 15:57:42 +0100

    console log

--- myscript.js
+++ myscript.js
@@ -1,4 +1,4 @@
-let scatola = document.getElementsByClassName("container");
+let scatola = document.getElementById("container");
 
 for(let i = 1; i <= 100; i++){
     console.log('iterazione: ', i);
