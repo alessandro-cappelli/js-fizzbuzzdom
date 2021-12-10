59a4f008583e87e37c36e7eae0f802dfafd1938e
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-10 17:14:20 +0100

    multipli di tre css color

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@ let scatola = document.getElementById("container");
 
 for(let i = 1; i <= 100; i++){
     if(i%3 == 0){ //multipli di 3
-        scatola.innerHTML += '<div class=box multipli-di-tre' + i + '</div>';
+        scatola.innerHTML += '<div class=box-3 square' + i + '</div>';
     }
     else{
         console.log('iterazione: ', i);
