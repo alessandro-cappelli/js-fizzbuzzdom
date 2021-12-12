8d584e98111f98757a567792f9ea649d6dc9cb40
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-12 18:28:35 +0100

    sistemata numerazione

--- myscript.js
+++ myscript.js
@@ -1,15 +1,18 @@
 let scatola = document.getElementById("container");
 
 for(let i = 1; i <= 100; i++){
-    if(i%3 == 0){ //multipli di 3
+    if((i%3 == 0) && (i%5 == 0)){ //multipli di 5 e di 3
+    scatola.innerHTML += '<div class=box-3-5 square>FizzBuzz</div>';
+    }
+
+    else if(i%3 == 0){ //multipli di 3
         scatola.innerHTML += '<div class=box-3 square>Fizz</div>';
     }
-    if(i%3 == 0){ //multipli di 5
+
+    else if(i%5 == 0){ //multipli di 5
         scatola.innerHTML += '<div class=box-5 square>Buzz</div>';
     }
-    if((i%3 == 0) && (i%5 == 0)){ //multipli di 5 e di 3
-        scatola.innerHTML += '<div class=box-3-5 square>FizzBuzz</div>';
-    }
+
     else{
         console.log('iterazione: ', i);
         scatola.innerHTML += '<div class=box square>' + i + '</div>';
